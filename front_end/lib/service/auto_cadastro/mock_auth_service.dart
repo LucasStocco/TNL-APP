5493314f5ad58a508d8444cb72d45d082c3a4e1d
LuizFVMarques <vieira.luiz@aluno.ifsp.edu.br>
2026-05-11 22:16:31 -0300

    FUNCIONANDO E SALVANDO (PRECISA ATUALIZAR OS DIAGRAMAS E O FLUTTER)

--- front_end/lib/service/auto_cadastro/mock_auth_service.dart
+++ front_end/lib/service/auto_cadastro/mock_auth_service.dart
@@ -1,67 +1,88 @@
-import 'package:crud_flutter/model/auto_cadastro/user.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
 import 'package:crud_flutter/model/auto_cadastro/user.dart';
+import 'package:crud_flutter/service/auto_cadastro/auth_service.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'auth_service.dart';
 
 class MockAuthService implements AuthService {
 
   static const _keyUser = 'user';
 
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: ['email'],
+  serverClientId:
+      '605363260040-q8s2e93017d9786n152lk4ufhm9gsibc.apps.googleusercontent.com',
+);
 
   @override
   Future<User?> login() async {
 
-    // Login Google
-    final GoogleSignInAccount? googleUser =
-        await _googleSignIn.signIn();
+    try {
 
-    if (googleUser == null) {
-      return null;
+      // LOGIN GOOGLE
+      final GoogleSignInAccount? googleUser =
+          await _googleSignIn.signIn();
+
+      print("googleUser: $googleUser");
+
+      if (googleUser == null) {
+        print("Usuário cancelou login");
+        return null;
+      }
+
+      // TOKEN GOOGLE
+      final GoogleSignInAuthentication auth =
+          await googleUser.authentication;
+
+      print("ID TOKEN: ${auth.idToken}");
+
+      final idToken = auth.idToken;
+
+      // CHAMADA BACKEND
+      final response = await http.post(
+        Uri.parse("http://172.16.89.60:8088/auth/google"),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: jsonEncode({
+          "idToken": idToken,
+        }),
+      );
+
+      print("STATUS BACKEND: ${response.statusCode}");
+      print("BODY BACKEND: ${response.body}");
+
+      if (response.statusCode != 200) {
+        throw Exception("Erro no backend");
+      }
+
+      final data = jsonDecode(response.body);
+
+      print(data);
+
+      final user = User(
+        id: data["id"],
+        nome: data["nome"] ?? "",
+        email: data["email"] ?? "",
+        fotoUrl: data["fotoUrl"],
+      );
+
+      // SALVA LOCALMENTE
+      final prefs = await SharedPreferences.getInstance();
+
+      await prefs.setString(_keyUser, user.nome);
+
+      return user;
+
+    } catch (e) {
+
+      print("ERRO GOOGLE LOGIN:");
+      print(e);
+
+      rethrow;
     }
-
-    // Pega token
-    final GoogleSignInAuthentication auth =
-        await googleUser.authentication;
-
-    final idToken = auth.idToken;
-
-    // Chama backend
-    final response = await http.post(
-      Uri.parse("http://192.168.0.10:8080/auth/google"),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: jsonEncode({
-        "idToken": idToken,
-      }),
-    );
-
-    if (response.statusCode != 200) {
-      throw Exception("Erro no backend");
-    }
-
-    final data = jsonDecode(response.body);
-
-    final user = User(
-      id: data["id"],
-      nome: data["nome"],
-      email: data["email"],
-      fotoUrl: data["fotoUrl"],
-    );
-
-    // salva nome localmente
-    final prefs = await SharedPreferences.getInstance();
-
-    await prefs.setString(_keyUser, user.nome);
-
-    return user;
   }
 
   @override
