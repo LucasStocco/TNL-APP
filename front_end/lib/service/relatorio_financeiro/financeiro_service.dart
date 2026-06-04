0cb42ee9c34c5b2ce4928fee43f00509c82e2e7d
wlb-f <wlb0980@gmail.com>
2026-06-04 14:51:10 -0300

    Atualizar o financeiro_service.dart (gasto por lista)

--- front_end/lib/service/relatorio_financeiro/financeiro_service.dart
+++ front_end/lib/service/relatorio_financeiro/financeiro_service.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:crud_flutter/core/api/api_config.dart';
-
 import '../../model/relatorio_financeiro/financeiro.dart';
 import '../../dto/gasto_total_response_dto.dart';
+import '../../dto/gasto_por_lista_response_dto.dart';
 
 class FinanceiroService {
   Future<GastoTotalModel> buscarTotalGeral() async {
@@ -31,4 +30,28 @@ class FinanceiroService {
       'Erro ao buscar total geral',
     );
   }
+Future<List<GastoPorListaModel>> buscarTotalPorLista() async {
+  final String url =
+      '${ApiConfig.baseUrl}/financeiro/total-por-lista';
+
+  final response = await http.get(
+    Uri.parse(url),
+  );
+
+  if (response.statusCode == 200) {
+    final json = jsonDecode(response.body);
+    final data = json['data'] as List;
+
+    return data.map((item) {
+      final dto = GastoPorListaResponseDTO.fromJson(item);
+
+      return GastoPorListaModel(
+        lista: dto.lista,
+        total: dto.total,
+      );
+    }).toList();
+  }
+
+  throw Exception('Erro ao buscar total por lista');
+}
 }
