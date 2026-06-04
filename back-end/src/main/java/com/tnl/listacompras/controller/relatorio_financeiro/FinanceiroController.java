b45fa8ebb47bbf752953f7b3ac6f793ac2753853
wlb-f <wlb0980@gmail.com>
2026-06-04 14:39:54 -0300

    Atualizar o FinanceiroController.java (total por lista)

--- back-end/src/main/java/com/tnl/listacompras/controller/relatorio_financeiro/FinanceiroController.java
+++ back-end/src/main/java/com/tnl/listacompras/controller/relatorio_financeiro/FinanceiroController.java
@@ -1,14 +1,15 @@
 package com.tnl.listacompras.controller.relatorio_financeiro;
 
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import com.tnl.listacompras.dto.responseDTO.relatorio_financeiro.GastoPorCategoriaDTO;
+import com.tnl.listacompras.dto.responseDTO.relatorio_financeiro.GastoPorListaDTO;
 import com.tnl.listacompras.dto.responseDTO.relatorio_financeiro.GastoTotalDTO;
 import com.tnl.listacompras.service.relatorio_financeiro.FinanceiroService;
+
 import response.ApiResponse;
 
 @RestController
@@ -22,47 +23,56 @@ public class FinanceiroController {
     ) {
         this.financeiroService = financeiroService;
     }
-    
-    
+
     @GetMapping("/listas/{id}/total")
     public ResponseEntity<ApiResponse<GastoTotalDTO>>
     calcularTotalLista(
             @PathVariable Long id
-            
     ) {
+
         GastoTotalDTO dto =
                 financeiroService.calcularTotalLista(id);
-        return ResponseEntity.ok(
-               ApiResponse.success(
-               "Total calculado",dto  ));
-    }
-    
-    
-    @GetMapping("/listas/{id}/categorias")
-    public ResponseEntity<
-            ApiResponse<List<GastoPorCategoriaDTO>>>
-            calcularPorCategoria(
-            @PathVariable Long id ) 
-    {	
-        List<GastoPorCategoriaDTO> dto =
-                financeiroService.calcularPorCategoria(id);
+
         return ResponseEntity.ok(
                 ApiResponse.success(
-                        "Gastos por categoria",  dto ) );
+                        "Total calculado",
+                        dto
+                )
+        );
     }
-    
-    
-    
+
+    @GetMapping("/listas/{id}/categorias")
+    public ResponseEntity<ApiResponse<List<GastoPorCategoriaDTO>>>
+    calcularPorCategoria(
+            @PathVariable Long id
+    ) {
+
+        List<GastoPorCategoriaDTO> dto =
+                financeiroService.calcularPorCategoria(id);
+
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        "Gastos por categoria",
+                        dto
+                )
+        );
+    }
+
     @GetMapping("/total-geral")
     public ResponseEntity<ApiResponse<GastoTotalDTO>>
-    	calcularTotalGeral() {
-    	
-    	GastoTotalDTO dto = 
-    			financeiroService.calcularTotalGeral();
-    	return ResponseEntity.ok(
-    			ApiResponse.success(
-    					"Total geral calculado", dto)); 			
+    calcularTotalGeral() {
+
+        GastoTotalDTO dto =
+                financeiroService.calcularTotalGeral();
+
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        "Total geral calculado",
+                        dto
+                )
+        );
     }
+
     @GetMapping("/media-gastos")
     public ResponseEntity<ApiResponse<GastoTotalDTO>>
     calcularMediaGastos() {
@@ -77,6 +87,19 @@ public class FinanceiroController {
                 )
         );
     }
-    
-    
+
+    @GetMapping("/total-por-lista")
+    public ResponseEntity<ApiResponse<List<GastoPorListaDTO>>>
+    calcularTotalPorLista() {
+
+        List<GastoPorListaDTO> dto =
+                financeiroService.calcularTotalPorLista();
+
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        "Total por lista calculado",
+                        dto
+                )
+        );
+    }
 }
