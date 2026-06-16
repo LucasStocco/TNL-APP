2cff06ca7720572ab34920a24fe5acd37d5bdbde
LuizFVMarques <vieira.luiz@aluno.ifsp.edu.br>
2026-06-16 10:18:39 -0300

    Filtro JWT

--- back-end/src/main/java/com/tnl/listacompras/config/SecurityConfig.java
+++ back-end/src/main/java/com/tnl/listacompras/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.tnl.listacompras.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,7 +25,11 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
-                .anyRequest().permitAll() // 🔥 temporário pra teste TOTAL
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(
+                jwtFilter,
+                UsernamePasswordAuthenticationFilter.class
             );
 
         return http.build();
